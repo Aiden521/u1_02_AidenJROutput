f4aa1e5b4a488c670731f9bfb6445776cc81b7ab
Aiden521 <36234220+Aiden521@users.noreply.github.com>
2018-02-09 09:35:37 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Aiden Jolley Ruhn
+ * Febuary 9, 2018
+ * Output Program
+ * User enters name and program outputs Hello name!
+ */ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace u1_02_AidenJrOutput
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btngreeting_Click(object sender, RoutedEventArgs e)
+        {
+            lbloutput.Content = "Hello, " + entryName.Text;
+        }
+    }
+}
